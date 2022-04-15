84864017d953242c989f463c42a75b153474da09
Hayley Chisholm <hayleychisholmk@Hayleys-MacBook-Pro.local>
2022-04-15 14:44:59 -0500

    Commiting my completed lab03 files

--- lab03/answers.sql
+++ lab03/answers.sql
@@ -10,38 +10,50 @@ FROM users;
 
 
 -- Exercise 3
-
+SELECT id, first_name, last_name 
+FROM users 
+ORDER BY last_name;
 
 
 
 -- Exercise 4
-
+SELECT id, user_id, image_url 
+FROM posts 
+WHERE user_id = 26;
 
 
 
 -- Exercise 5
-
+SELECT id, image_url, user_id 
+FROM posts 
+WHERE user_id = 26 OR user_id = 12;
 
 
 
 -- Exercise 6
 
-
+SELECT count(*) 
+FROM posts;
 
 
 -- Exercise 7
+
+SELECT user_id, count(*) 
+FROM comments 
+GROUP BY user_id 
+ORDER BY count(*) DESC;
 
 
 
 
 -- Exercise 8
-
+SELECT posts.id, posts.image_url, posts.user_id, users.username, users.first_name, users.last_name FROM posts INNER JOIN users ON posts.user_id = users.id WHERE user_id=26 OR user_id=12;
 
 
 
 -- Exercise 9
 
-
+SELECT posts.id, posts.pub_date, following.following_id FROM posts INNER JOIN following ON posts.user_id = following.following_id WHERE posts.user_id = 26;
 
 
 -- Exercise 10
